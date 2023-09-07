4b3977c3c3f2f8ccfbd965ba9ada9b933c7cb062
saile515 <elias.jorgensen2006@gmail.com>
2023-09-07 16:59:10 +0200

    fix lsp issue

--- init.lua
+++ init.lua
@@ -8,12 +8,6 @@ vim.api.nvim_command('set shiftwidth=4')
 vim.api.nvim_command('set expandtab')
 vim.keymap.set("n", "<C-s>", ":w<Enter>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<Enter>li")
-vim.keymap.set("n", "<F5>", ":!cmake --build bin --parallel -t Server && ./bin/Server<Enter>")
-
-vim.cmd("autocmd VimEnter * cd ~/Documents/projects")
-vim.cmd("autocmd VimEnter * CHADopen --nofocus")
-vim.cmd("autocmd VimEnter * COQnow --shut-up")
-
 vim.cmd[[colorscheme vscode]]
 
 require('lualine').setup()
@@ -22,19 +16,23 @@ require('gitsigns').setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
-	function (server_name)
-            require("lspconfig")[server_name].setup {}
-        end
+    function (server_name)
+        require("lspconfig")[server_name].setup {}
+    end
 }
 
 require "lspconfig".clangd.setup {
-  settings = {
-     clangd = {
-	arguments = {"-std=c++20"}
-     }
-  }
+    settings = {
+        clangd = {
+            arguments = {"-std=c++20"}
+        }
+    }
 }
 
 require("null-ls").setup()
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.cmd("autocmd VimEnter * cd ~/Documents/projects")
+vim.cmd("autocmd VimEnter * CHADopen --nofocus")
+vim.cmd("autocmd VimEnter * COQnow --shut-up")
