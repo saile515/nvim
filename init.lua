bee1a72a11b2ca637f34de0bf397e29e437cd8d1
saile515 <elias.jorgensen2006@gmail.com>
2023-10-02 14:09:11 +0200

    remove duplicate lines

--- init.lua
+++ init.lua
@@ -7,18 +7,9 @@ vim.api.nvim_command('set tabstop=4')
 vim.api.nvim_command('set shiftwidth=4')
 vim.api.nvim_command('set expandtab')
 vim.keymap.set("n", "<C-s>", ":w<Enter>")
-vim.keymap.set("i", "<C-s>", "<Esc>:w<Enter>li")
-<<<<<<< HEAD
-vim.cmd[[colorscheme vscode]]
-=======
-vim.keymap.set("n", "<F5>", ":!cmake --build bin --parallel")
-
-vim.cmd("autocmd VimEnter * cd ~/Documents/projects")
-vim.cmd("autocmd VimEnter * CHADopen --nofocus")
-vim.cmd("autocmd VimEnter * COQnow --shut-up")
+vim.keymap.set("i", "<C-s>", "<Esc>:w<Enter>")
 
 vim.cmd.colorscheme "catppuccin-macchiato"
->>>>>>> ed4c0560763612921fc1cc685d66c280c1add309
 
 require('lualine').setup()
 require('gitsigns').setup()
