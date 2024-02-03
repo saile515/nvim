71a90f22ebcf913f0a60aa7ad1022f01afcbe186
saile515 <elias.jorgensen2006@gmail.com>
2024-02-03 11:43:16 +0100

    switch to lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,39 +1,29 @@
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-    use "wbthomason/packer.nvim"
-
-    use {"williamboman/mason.nvim", run = ":MasonUpdate"}
-
-    use "williamboman/mason-lspconfig.nvim"
-    use "neovim/nvim-lspconfig"
-    use "mfussenegger/nvim-dap"
-    use {"mhartington/formatter.nvim"}
-    use "mfussenegger/nvim-lint"
-
-    use {"ms-jpq/chadtree", branch = "chad", run = "python3 -m chadtree deps"}
-
-    use {"ms-jpq/coq_nvim", branch = "coq"}
-
-    use {"ms-jpq/coq.artifacts", branch = "artifacts"}
-
-    use {"ms-jpq/coq.thirdparty", branch = "3p"}
-
-    use {"catppuccin/nvim", as = "catppuccin"}
-
-    use {
+return require("lazy").setup({
+    {"williamboman/mason.nvim"},
+    "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap", {"mhartington/formatter.nvim"},
+    "mfussenegger/nvim-lint",
+    {"ms-jpq/chadtree", branch = "chad"},
+    {"ms-jpq/coq_nvim", branch = "coq"},
+    {"ms-jpq/coq.artifacts", branch = "artifacts"},
+    {"ms-jpq/coq.thirdparty", branch = "3p"},
+    {"catppuccin/nvim", name = "catppuccin"},
+    {
         "nvim-lualine/lualine.nvim",
-        requires = {"nvim-tree/nvim-web-devicons", opt = true}
+        dependencies = {"nvim-tree/nvim-web-devicons"}
+    }, {"lewis6991/gitsigns.nvim", tag = "release"}, "folke/trouble.nvim", {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {"nvim-lua/plenary.nvim"}
     }
-
-    use {"lewis6991/gitsigns.nvim", tag = 'release'}
-
-    use "folke/trouble.nvim"
-
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = {{'nvim-lua/plenary.nvim'}}
-    }
-end)
+})
